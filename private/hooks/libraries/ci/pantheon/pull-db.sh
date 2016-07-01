22c70c2fc773b435c527de2e81f954a76346963a
William O'Connor <bill@achieveinternet.com>
2016-07-01 14:45:15 -0700

    Update script.

--- private/hooks/libraries/ci/pantheon/pull-db.sh
+++ private/hooks/libraries/ci/pantheon/pull-db.sh
@@ -2,5 +2,5 @@
 
 # Authenticate to Terminus and pull our DB from Pantheon.
 terminus auth login --machine-token=$MACHINETOKEN
-terminus drush status --site=$PAASSITE --env=$PAASENV
-terminus drush sql-dump --site=$PAASSITE --env=$PAASENV > db.sql
+echo "terminus drush 'sql-dump' --site=$PAASSITE --env=$PAASENV"
+`terminus drush "sql-dump" --site=$PAASSITE --env=$PAASENV` > db.sql
